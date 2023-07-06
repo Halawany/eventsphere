9b4b13293fbd13e86a65bd88a9d029ca830d88fb
Eslam Halawany <halawany.me@gmail.com>
2023-07-06 18:37:36 +0300

    Added rest_framework to installed apps

--- eventsphere/settings.py
+++ eventsphere/settings.py
@@ -26,6 +26,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd party apps
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
